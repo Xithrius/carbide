5d801c1977f39bdb2c36637790f36b1dcf71735d
Xithrius <xithrius@gmail.com>
2022-12-28 17:55:46 -0800

    Appeased pre-commit

--- app/run.py
+++ app/run.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 from prisma import Prisma
 
 app = FastAPI()
@@ -7,16 +8,22 @@ prisma = Prisma(auto_register=True)
 
 
 @app.get("/")
-async def root():
+async def root() -> None:
+    """GET API Request with no path."""
     return {"message": "Your app is working!"}
 
 
 @app.on_event("startup")
 async def startup() -> None:
+    """Items to be executed before the application starts up."""
     await prisma.connect()
 
 
 @app.on_event("shutdown")
 async def shutdown() -> None:
+    """Items to be executed when the application is to shut down."""
     if prisma.is_connected():
         await prisma.disconnect()
+
+
+# https://fastapi.tiangolo.com/tutorial/testing/?h=testing#testing
